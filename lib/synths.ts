1cf640039210013e8abd5b662376e209df825ce6
zubizuber <brodu.olivier.contact@gmail.com>
2026-09-03 08:31:29 +0200

    Fix Synthpedia data for Vercel
    * Add Synthpedia data part 1 of 16
    
    * Add Synthpedia data part 2 of 16
    
    * Add Synthpedia data part 3 of 16
    
    * Add Synthpedia data part 4 of 16
    
    * Add Synthpedia data part 5 of 16
    
    * Add Synthpedia data part 6 of 16
    
    * Add Synthpedia data part 7 of 16
    
    * Add Synthpedia data part 8 of 16
    
    * Add Synthpedia data part 9 of 16
    
    * Add Synthpedia data part 10 of 16
    
    * Add Synthpedia data part 11 of 16
    
    * Add Synthpedia data part 12 of 16
    
    * Add Synthpedia data part 13 of 16
    
    * Add Synthpedia data part 14 of 16
    
    * Add Synthpedia data part 15 of 16
    
    * Add Synthpedia data part 16 of 16
    
    * Load chunked Synthpedia data
    
    * Load chunked Synthpedia data
    
    * Assemble Synthpedia data parts
    
    * Remove corrupted monolithic data file

---
+++ lib/synths.ts
@@ -0,0 +1,37 @@
+import part01 from "./synths-01.json";
+import part02 from "./synths-02.json";
+import part03 from "./synths-03.json";
+import part04 from "./synths-04.json";
+import part05 from "./synths-05.json";
+import part06 from "./synths-06.json";
+import part07 from "./synths-07.json";
+import part08 from "./synths-08.json";
+import part09 from "./synths-09.json";
+import part10 from "./synths-10.json";
+import part11 from "./synths-11.json";
+import part12 from "./synths-12.json";
+import part13 from "./synths-13.json";
+import part14 from "./synths-14.json";
+import part15 from "./synths-15.json";
+import part16 from "./synths-16.json";
+
+const synths = [
+  ...part01,
+  ...part02,
+  ...part03,
+  ...part04,
+  ...part05,
+  ...part06,
+  ...part07,
+  ...part08,
+  ...part09,
+  ...part10,
+  ...part11,
+  ...part12,
+  ...part13,
+  ...part14,
+  ...part15,
+  ...part16,
+];
+
+export default synths;
