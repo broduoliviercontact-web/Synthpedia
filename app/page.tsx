d86f209b5bd63fae6c79072d62f805863b68cd39
zubizuber <brodu.olivier.contact@gmail.com>
2026-09-03 08:30:46 +0200

    Load chunked Synthpedia data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import SynthpediaExplorer from "@/components/synthpedia-explorer";
-import synths from "@/lib/synths.json";
+import synths from "@/lib/synths";
 
 export default function Home() {
   return <SynthpediaExplorer synths={synths} />;
